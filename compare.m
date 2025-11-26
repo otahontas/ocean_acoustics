888299cafa0870c5945c44dc825c3b6fbc4896ee
Otto Ahoniemi <otto@ottoahoniemi.fi>
2025-11-26 02:55:17 +0200

    Cleanup

--- compare.m
+++ compare.m
@@ -1,9 +1,13 @@
-%% Compare C-Linear vs Bellhop eigenrays
+%% Compare ray tracing models vs Bellhop
 clear; close all; clc;
 
-%% Run C-Linear model
-fprintf('Running C-Linear model...\n');
-run_clinear;
+%% Run C-Linear Curvature model
+fprintf('Running C-Linear Curvature model...\n');
+clinear_curvature;
+
+%% Run Ray Parameter model
+fprintf('Running Ray Parameter model...\n');
+ray_parameter;
 
 %% Run Bellhop
 fprintf('Running Bellhop...\n');
